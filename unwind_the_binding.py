ba86a345b91fe34c0c7740a0bb9986e89a64d0b4
David Black <22400988+bballdave025@users.noreply.github.com>
2024-02-20 14:54:24 -0700

    Incremental saves

--- unwind_the_binding.py
+++ unwind_the_binding.py
@@ -29,8 +29,8 @@ from tqdm import tqdm
 
 #just_one_pdf_at_a_time = True
 
-fromdir = "C:/Users/Anast/Desktop/_waiting_pdfs"
-outdir = "C:/Users/Anast/Desktop/_waiting_wild_imgs_out"
+fromdir = "C:/Users/bballdave025/Desktop/_waiting_pdfs"
+outdir = "C:/Users/bballdave025/Desktop/_waiting_wild_imgs_out"
 
 for each_path in os.listdir(fromdir):
   if ".pdf" in each_path:
